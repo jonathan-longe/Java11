693d3a783f7208cc153ad90e9c8eff22c6db3108
Alex L <alexl@pop-os.localdomain>
2021-10-24 14:40:56 -0700

    switch template and chap 16 ex 1

--- src/Chap16/Chap16Ex1.java
+++ src/Chap16/Chap16Ex1.java
@@ -1,4 +1,28 @@
-package Chap16;
+import java.util.Scanner;
+class Chap16Ex1
+{
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
 
-public class Chap16Ex1 {
+        int intToAdd;
+        int count = 0;
+        int number = 0;
+        int numberAdded;
+
+        System.out.println("Enter how many integers will be added:");
+        intToAdd = scan.nextInt();
+
+        while (count != intToAdd) {
+            System.out.println("Enter an integer:");
+            numberAdded = scan.nextInt();
+            number = number + numberAdded;
+            count ++;
+
+        }
+
+        System.out.println("The sum is: " + number); // final answer
+
+
+    }
 }
--- src/Template1.java
+++ src/Template1.java
@@ -1,28 +1,9 @@
 import java.util.Scanner;
-class Chap16Ex1
+class Template
 {
     public static void main (String[] args)
     {
         Scanner scan = new Scanner(System.in);
-
-        int intToAdd;
-        int count = 0;
-        int number = 0;
-        int numberAdded;
-
-        System.out.println("Enter how many integers will be added:");
-        intToAdd = scan.nextInt();
-
-        while (count != intToAdd) {
-            System.out.println("Enter an integer:");
-            numberAdded = scan.nextInt();
-            number = number + numberAdded;
-            count ++;
-
-        }
-
-        System.out.println("The sum is: " + number); // final answer
-
 
     }
 }
