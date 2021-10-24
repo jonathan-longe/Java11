a44e8df0fd6113dafb6bb01c31e3ee84b9c9a300
Alex L <alexl@pop-os.localdomain>
2021-10-24 14:34:57 -0700

    chapter 16 ex 1

--- src/Template1.java
+++ src/Template1.java
@@ -1,9 +1,28 @@
 import java.util.Scanner;
-class Echo
+class Chap16Ex1
 {
     public static void main (String[] args)
     {
         Scanner scan = new Scanner(System.in);
+
+        int intToAdd;
+        int count = 0;
+        int number = 0;
+        int numberAdded;
+
+        System.out.println("Enter how many integers will be added:");
+        intToAdd = scan.nextInt();
+
+        while (count != intToAdd) {
+            System.out.println("Enter an integer:");
+            numberAdded = scan.nextInt();
+            number = number + numberAdded;
+            count ++;
+
+        }
+
+        System.out.println("The sum is: " + number); // final answer
+
 
     }
 }
