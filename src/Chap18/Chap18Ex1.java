a554e46cb1ae6ca3b80af1374bd26a03e8dcb3b9
PumpkinSqiggle <alex.m.longe@gmail.com>
2021-10-26 09:21:17 -0700

    Added Chap 18

---
+++ src/Chap18/Chap18Ex1.java
@@ -0,0 +1,46 @@
+package Chap18;
+
+import java.util.Scanner ;
+
+class Chap18Ex1
+
+{
+
+    public static void main (String[] args )
+
+    {
+
+        Scanner scan=new Scanner( System.in );
+
+        double initialmiles,finalmiles,gallons ;
+
+        System.out.print ("Enter initial miles: ");
+
+        initialmiles = scan.nextDouble ();
+
+        while (!( initialmiles ==-1)) //keep going while initialmiles is NOT negative 1
+
+        {
+
+            System.out.print ("Enter Final Miles: ");
+
+            finalmiles = scan.nextDouble ();
+
+            System.out.print ("Enter gallons: ");
+
+            gallons= scan.nextDouble ();
+
+            System.out.println ("Miles per Gallon: "+( finalmiles-initialmiles )/gallons);
+
+            System.out.println ();
+
+            System.out.print ("Enter initial miles (-1 to exit): ");
+
+            initialmiles = scan.nextDouble ();
+
+        }
+
+        System.out.println ("Bye");
+
+    }
+
